62f2b73a6216e22c90828d84c1e899dbdc702bd5
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-04-22 09:49:46 +0200

    Question 2

--- Program.cs
+++ Program.cs
@@ -6,16 +6,33 @@
         {
             #region Question 1
 
-           //            Static Binding
-           //بيحصل وقت الـ Compile
-           //زي: Method Overloading
-           //أسرع
-           //Dynamic Binding
-           //بيحصل وقت الـ Runtime
-           //زي: Method Overriding
-           //بيعتمد على نوع الـ object الحقيقي
+            //            Static Binding
+            //بيحصل وقت الـ Compile
+            //زي: Method Overloading
+            //أسرع
+            //Dynamic Binding
+            //بيحصل وقت الـ Runtime
+            //زي: Method Overriding
+            //بيعتمد على نوع الـ object الحقيقي
 
             #endregion
+
+            #region Question 2
+
+            //Overloading
+            /*نفس الاسم بس parameters مختلفة
+             * في نفس الكلاس
+             * Compile time
+             */
+            //Overriding
+            /*
+             * نفس الاسم ونفس التوقيع
+             * في inheritance
+             * Runtime
+             */
+
+            #endregion
+
         }
     }
 }
